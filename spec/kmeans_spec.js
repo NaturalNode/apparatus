2c3e7d2348af31b82f094baa6c29cebbab91ba8c
Chris Umbel <chris@chrisumbel.com>
2012-02-16 23:33:02 -0500

    merged in sys->util. removed k-means specs for now

--- spec/kmeans_spec.js
+++ spec/kmeans_spec.js
@@ -23,19 +23,4 @@ THE SOFTWARE.
 var KMeans = require('.././lib/apparatus/clusterer/kmeans');
 
 describe('kmeans', function() {
-    it('should perform binary clustering', function() {
-	var X = $M([[1, 1], [2, 1], [4, 3], [5, 4]]);
-	var k = 2;
-	var kmeans = new KMeans(X, k);
-
-	expect(kmeans.cluster(2).eql($V([1, 1, 2, 2]))).toBeTruthy();
-    });
-
-    it('should cluster', function() {
-	var X = $M([[1, 1], [2, 1], [4, 3], [5, 4], [11, 12], [11, 13]]);
-	var k = 3;
-	var kmeans = new KMeans(X, k);
-
-	expect(kmeans.cluster(3).eql($V([1, 1, 2, 2, 3, 3]))).toBeTruthy();
-    });
 });
