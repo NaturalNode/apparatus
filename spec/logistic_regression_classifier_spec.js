eb373963e4ce2bff9a3fb3f344a1527483f31e1b
Dominik Finkbeiner <dominik.finkbeiner@sap.com>
2018-04-12 12:49:57 +0200

    handle infinite loop #13
    https://github.com/NaturalNode/natural/issues/309
    https://github.com/NaturalNode/natural/issues/382

--- spec/logistic_regression_classifier_spec.js
+++ spec/logistic_regression_classifier_spec.js
@@ -22,7 +22,7 @@ THE SOFTWARE.
 
 var LogisticRegressionClassifier = new require('../lib/apparatus/classifier/logistic_regression_classifier');
 
-describe('logistic regression', function() {    
+describe('logistic regression', function() {
     it('should classify with examples added in groups', function() {
         var logistic = new LogisticRegressionClassifier();
         logistic.addExample([1,1,1,0,0,0], 'one');
@@ -33,7 +33,7 @@ describe('logistic regression', function() {
         logistic.addExample([0,0,0,1,1,0], 'two');
 
         logistic.train();
-        
+
         expect(logistic.classify([0,1,1,0,0,0])).toBe('one');
         expect(logistic.classify([0,0,0,0,1,1])).toBe('two');
     });
@@ -97,5 +97,22 @@ describe('logistic regression', function() {
         expect(newLogistic.classify([1,1,0,0,0,0,1,0,0])).toBe('one');
         expect(newLogistic.classify([0,0,1,1,1,0,0,0,1])).toBe('two');
         expect(newLogistic.classify([1,0,0,0,1,0,0,1,1])).toBe('three');
+    });
+
+    it('should not run into an infinite loop (1)', function () {
+        var logistic = new LogisticRegressionClassifier();
+        logistic.addExample([1, 1, 1, 0, 0, 0, 0, 0, 0], 'one');
+        logistic.addExample([1, 1, 1, 0, 0, 0, 0, 0, 0], 'two');
+
+        logistic.train();
+    });
+
+    it('should not run into an infinite loop (2)', function () {
+        var logistic = new LogisticRegressionClassifier();
+        logistic.addExample([1, 1, 1, 0, 0, 0, 0, 0, 0], 'one');
+        logistic.addExample([1, 1, 1, 0, 0, 0, 0, 0, 0], 'two');
+        logistic.addExample([1, 1, 1, 0, 0, 0, 0, 0, 0], 'three');
+
+        logistic.train();
     });
 });
